964f3aa50b460b27244fb5976e997d31502420a0
Remo Mattei <remo@mattei.org>
2020-04-08 20:44:38 -0700

    change to echo

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -18,4 +18,4 @@ echo 'application renders satisfactorily. This command actually invokes the test
 echo 'runner Jest (https://facebook.github.io/jest/).'
 set -x
 # npm test
-ping -c 4 google.com
+echo 'Ciao Remo Mattei'
