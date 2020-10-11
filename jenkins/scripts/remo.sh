554d9bfc866a1fdf7bffb82b5e1db6a257c90d58
Remo Mattei <remo@mattei.org>
2020-10-11 00:10:07 -0700

    adding unzip path

--- jenkins/scripts/remo.sh
+++ jenkins/scripts/remo.sh
@@ -1,4 +1,4 @@
 echo "Ciao Remo"
 curl https://google.com
 curl -o terraform_0.13.4.zip  https://releases.hashicorp.com/terraform/0.13.4/terraform_0.13.4_linux_amd64.zip 
-unzip -d /tmp/terraform /tmp/terraform_0.13.4.zip
+/usr/bin/unzip -d /tmp/terraform /tmp/terraform_0.13.4.zip
