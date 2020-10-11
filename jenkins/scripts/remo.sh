a0f8cbaa169fe43698b0dc1dd74fb81d86e66a68
Remo Mattei <remo@mattei.org>
2020-10-11 00:35:21 -0700

    adding ciao

--- jenkins/scripts/remo.sh
+++ jenkins/scripts/remo.sh
@@ -1,2 +1,3 @@
 curl -o terraform_0.13.4.zip  https://releases.hashicorp.com/terraform/0.13.4/terraform_0.13.4_linux_amd64.zip 
-sh "/usr/bin/unzip -d /tmp/terraform /tmp/terraform_0.13.4.zip"
+bash -c /usr/bin/unzip -d /tmp/terraform /tmp/terraform_0.13.4.zip
+echo "ciao"
