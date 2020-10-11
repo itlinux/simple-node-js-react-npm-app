daa0c82ad37d0ca7d94364cedc40ada6f01b0e00
Remo Mattei <remo@mattei.org>
2020-10-11 00:22:16 -0700

    remove curl google

--- jenkins/scripts/remo.sh
+++ jenkins/scripts/remo.sh
@@ -1,4 +1,2 @@
-echo "Ciao Remo"
-curl https://google.com
 curl -o terraform_0.13.4.zip  https://releases.hashicorp.com/terraform/0.13.4/terraform_0.13.4_linux_amd64.zip 
 sh "/usr/bin/unzip -d /tmp/terraform /tmp/terraform_0.13.4.zip"
